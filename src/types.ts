fabd412d654fce41491a9da522241c0c83597bff
Eduardo Damasceno <damasceno.eduardo85@gmail.com>
2024-06-04 18:06:35 -0300

    update types

--- src/types.ts
+++ src/types.ts
@@ -13,3 +13,16 @@ export interface Booking {
   id: string;
   dateRange: DateRange;
 }
+
+export interface UseBookingFormReturn {
+  formData: Booking;
+  totalNights: number | null;
+  handleSubmit: (e: React.FormEvent<Element>) => (() => void) | undefined;
+  setFormData: React.Dispatch<React.SetStateAction<Booking>>;
+  handleRangeChange: (range: DateRange) => void;
+  handlePropertyChange: (val: string | undefined) => void;
+  errorMessage: string | undefined;
+  setErrorMessage: React.Dispatch<React.SetStateAction<string | null>>;
+  successMessage: string | undefined;
+  setSuccessMessage: React.Dispatch<React.SetStateAction<string | null>>;
+}
